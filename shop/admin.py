6a1bd3ab0232bf1d318373819d2ab0399e09c893
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-03-31 22:28:00 +0530

    'pul'

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from . models import spices
+from . models import pulses
 # Register your models here.
 
 
 admin.site.register(spices)
+admin.site.register(pulses)
