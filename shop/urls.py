ac6f68a9d4e517646e1c8afc40a9c86f1e86e4e1
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-03-31 23:35:06 +0530

    'comma'

--- shop/urls.py
+++ shop/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('spice',views.catspices, name='catspices'),
-    path('pulse',views.catpulse, name='catpulse')
-    path('oil',views.catpulse, name='catoil')
+    path('pulse',views.catpulse, name='catpulse'),
+    path('oil',views.catpulse, name='catoil'),
     path('veg',views.catpulse, name='catveg')
 ]
