bb29f494260479990d95b44aa82c3925cf415e94
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-03-31 05:55:43 +0530

    'registerpage'

--- first/urls.py
+++ first/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('greens.urls')),
     path('admin/', admin.site.urls),
+    path('account/', include('account.urls'))
 ]
 
 
