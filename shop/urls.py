df9d5a19f1c34bb57c076c389a02a30ad88fdb17
Samiksha Agrawal <agrawalsamiksha25@gmail.com>
2020-03-31 23:31:45 +0530

    'oilveg'

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.catspices, name='catspices'),
-    path('',views.catpulse, name='catpulse')
+    path('spice',views.catspices, name='catspices'),
+    path('pulse',views.catpulse, name='catpulse')
+    path('oil',views.catpulse, name='catoil')
+    path('veg',views.catpulse, name='catveg')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from . models import spices
 from . models import pulses
+from . models import oils
+from . models import vegs
 
 # Create your views here.
 
@@ -14,3 +16,13 @@ def catpulse(request):
     pulse = pulses.objects.all()
 
     return render(request, "catpulse.html", {'pulse': pulse})
+
+def catoil(request):
+    oil = oils.objects.all()
+
+    return render(request, "catoil.html", {'oil': oil})    
+
+def catveg(request):
+    veg = vegs.objects.all()
+
+    return render(request, "catveg.html", {'veg': veg})    
